f0aa92c3662e265217ec4b7b70b687f71a470c78
Leosce <121965527+Leosce@users.noreply.github.com>
2025-04-17 23:24:15 +0200

    Add files via upload

--- LLMapp.py
+++ LLMapp.py
@@ -1,17 +1,97 @@
 import streamlit as st
-from LLMpy import classify
+from LLM import classify, summarize, tag, read, generate, translate, unmask
 
-st.title('Sentiment Classification')
+st.title('Multi-function NLP App')
 
-st.header("Please enter the text you would like to classify.")
+st.write("What would you like to do?")
+
+options = ['Classify', 'Summarize', 'Tag', 'Q&A', 'Generate', 'Translate', 'Unmask']
+
+option = st.selectbox("Choose an option:", options)
+
+if option == 'Generate':
+    st.write('Please note that the maximum allowed input for the generator is 100 tokens.')
+
+st.header("Please enter the text below.")
 
 user_input = st.text_input("Enter your text here: ")
 
-if user_input:
-    output = classify(user_input)
-    
-    st.write("")
-    
-    st.write('##### {}'.format('Classification for the provided text: ',))
+question=''
+if option =='Q&A':
+    question = st.text_input("Please enter what you're looking for in the text: ")
 
-    st.write('## {}'.format(output[0]['label']+':&nbsp;&nbsp;'+str(output[0]['score']*100)+'%'))
+language=''
+if option == "Translate":
+    language = st.text_input("Enter target language (e.g., French, German):")
+
+if st.button("Run"):
+
+    if option == 'Classify':
+        
+        output = classify(user_input)
+        
+        st.write("")
+        
+        st.write('##### {}'.format('Classification for the provided text: ',))
+
+        st.write('## {}'.format(output[0]['label']+':&nbsp;&nbsp;'+str(output[0]['score']*100)+'%'))
+
+    elif option == 'Summarize':
+        
+        output = summarize(user_input)
+        
+        st.write("")
+        
+        st.write('##### {}'.format('Summerization for the provided text: ',))
+        
+        st.write(output)
+        
+    elif option == 'Tag':
+        
+        output = tag(user_input)
+        
+        st.write("")
+        
+        st.write('##### {}'.format('Tagging for the provided text: ',))
+        
+        for result in output:
+            st.write(result)
+            
+    elif option == 'Q&A':
+        
+        output = read(user_input, question)
+        
+        st.write("")
+        
+        st.write('##### {}'.format('Answer for the provided lookup and text: ',))
+        
+        st.write(output)
+
+    elif option == 'Generate':
+        output = generate(user_input)
+        
+        st.write("")
+        
+        st.write('##### {}'.format('Generated text: ',))
+        
+        st.write(output)
+        
+    elif option == 'Translate':
+        output = translate(user_input,language)
+        
+        st.write("")
+        
+        st.write('##### {}'.format('Translated text: ',))
+        
+        st.write(output)
+        
+    elif option == 'Unmask':
+        
+        output = unmask(user_input)
+        
+        st.write("")
+        
+        st.write('##### {}'.format('Answer for the provided mask: ',))
+        
+        for result in output:
+            st.write(result)
